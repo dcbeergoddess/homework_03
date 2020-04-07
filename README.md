f86ae4b0b427e6579fe309b1f2d55750da86824e
Rachel Murray <dcbeergoddess@gmail.com>
2020-04-06 21:59:34 -0400

    setting up loops for choosePassword

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# homework_03
+# Homework_03 JavaScript Password Generator
+  Homework Due Monday April 6th 12am
+
+## Pseudo Code from Homework README Acceptance Criteria
+
+  ```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+------Maybe and object within an object??? below
+THEN I am presented with a series of prompts for password criteria
+    Make confirm statements to ask what user wants
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+
+
+    code a prompt that asks the user for how many characters in the passwords must be between 8 and 128
+    store response in variable
+
+
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+
+      do a confirm for lowercase
+      confirm for uppercase
+      confirm for numeric
+      confirm for special characters
+      do a varaible for each, conditional statements
+
+
+
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+
+
+    before you start generating passwords, need to create arrays for lowercase letters, uppercase, numbers, special characters, do this at the very beginning 
+
+    create and empty an array that will hold characters allowed in the solution. building final array that includes the options. 
+
+    If user choose lowercase
+      add all lowercase options to that array
+    If user choose uppercase
+      add all uppercase options ot that array
+  
+    We end up with ONE BIG ARRAY that holds all possible characters in the solution. 
+
+    If they choose 100 as the lengeth. you have to grab 100 times...
+    Create an empty string for the final password
+    loop from 1 to the number of characters in the password // ex 100
+      Determine a random number between 0 and the length-1 of our ONE BIG ARRAY (random index number out of our array between 0 and 299)
+
+      Whatever that random index is, grab that value and add it to the password string
+
+
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+
+
+## Notes From TAs for Completing Homework
+          // Do not need to worry about changing dom.... 
+      // NEED have to have function generatePassword 
+          // do you pass anything in??
+          // consider using more than one funciton and calling functions within funcitons\
+          // maybe use while loops
+          // use for loops (looking in Erik's google doc)
+
+          // write simple functions. console.log in to make sure that things are working
+          // do little increments and then test that functionality
+          // TEST THE LITTLE THINGS
+
+      // PSA have homework instruction right next to!!! pay attention to user story is part of acceptance criteria
+      // maybe create a random  function
+      // make very long functions and maybe go back and condense
+      // Interaction with the DOM (code given by homework it is already there but read up on it)
+      // generate button linking between html and js. FYI. 
+      // only writing js code for this assignment
+
+      // Walk through solutions in lessons and understand what is going on
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,21 @@
+// Create Arrays with different character sets include empty one for allowed characters
+var allowedCharacters = []
+var number = ["0", "1", "2","3","4","5","6","7","8","9"]
+console.log(number)
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+console.log(upperCase)
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+console.log(lowerCase)
+//backslash "\"" did not work as special character in array. characters taken from resource provided in homework ReadMe
+var specialCharacters = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",";",":","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+console.log(specialCharacters)
+
+console.log(allowedCharacters);
+console.log(number.length);
+console.log(upperCase.length);
+console.log(lowerCase.length);
+console.log(specialCharacters.length);
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -6,59 +24,59 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  console.log(password)
+
   passwordText.value = password;
 
 }
 
-// picking critiers options
-
-function getPasswordOptions () {
-  // convert string into number, parsing
-  var passwordLength = parseInt(prompt("How may characters would you like your password to be?"));
-   // limit length of password
-  if(passwordLength < 8 || passwordLength > 128) {
-    alert("Pawwso")
-  } 
-
-}
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Create arrays with differnt character sets
+// picking criteria options
+// First ask for length then criteria options
 
-var number = ["0", "1", "2","3","4","5","6","7","8","9"]
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialCharacters = ["!","@","#","$","%","^","&","(",")","-","_","+","=","|",";",":"];
+function getPasswordOptions () {
+  // Get User input
+  // convert string into number, parsing, from Dan example
+  var passwordLength = parseInt(prompt("How may characters would you like your password to be?"));
+   // limit length of password
+  if(passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8 and 128 characters")
+  } 
+  console.log(passwordLength)
 
-console.log(number);
-console.log(upperCase);
-console.log(lowerCase);
-console.log(specialCharacters);
-
-// Do not need to worry about changing dom.... 
-// NEED have to have function generatePassword 
-    // do you pass anything in??
-    // consider using more than one funciton and calling functions within funcitons\
-    // maybe use while loops
-    // use for loops (looking in Erik's google doc)
-
-    // write simple functions. console.log in to make sure that things are working
-    // do little increments and then test that functionality
-    // TEST THE LITTLE THINGS
-
-// PSA have homework instruction right next to!!! pay attention to user story is part of acceptance criteria
-// maybe create a random  function
-// make very long functions and maybe go back and condense
-// Interaction with the DOM (code given by homework it is already there but read up on it)
-// generate button linking between html and js. FYI. 
-// only writing js code for this assignment
-
-// Walk through solutions in lessons and understand what is going on
-
-
-
-
+  var chooseLowerCase = confirm("Do you want your password to include lowercase letters?");
+  var chooseUpperCase = confirm("Do you want your password to include uppercase letters?");
+  var chooseNumber = confirm("Do you want your password to include numbers?");
+  var chooseSpecialCharacters = confirm("Do you want your password to include special characters?");
+    
+  // lowerCase Loop
+  if(chooseLowerCase){
+    for(var i = 0; i < lowerCase.length; i++){
+      console.log(i);
+    }
+  }
+    
+  // upperCase Loop
+  if(chooseUpperCase){
+    for(var i = 0; i < upperCase.length; i++){
+      console.log(i);
+    }
+  }
+    
+  // number loop
+  if(chooseNumber){
+    for(var i = 0; i < number.length; i++){
+      console.log(i);
+    }
+  }
+    
+  // specialCharacters loop
+  if(chooseSpecialCharacters){
+    for(var i = 0; i < specialCharacters.length; i++){
+      console.log(i);
+    }
+  }
+}
 
